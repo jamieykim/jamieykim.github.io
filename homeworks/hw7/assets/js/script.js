d288a2822f4f4e24651885d5e3d9db2c796dfe70
jamie kim <Jamiekim@jamies-macbook.local>
2022-11-03 19:09:01 -0700

    finished hw7

---
+++ homeworks/hw7/assets/js/script.js
@@ -0,0 +1,21 @@
+function averageThreeNumbers (a, b, c) {
+    let sum = a + b + c;
+    let average = sum/3;
+    return average;
+}
+
+function createSentence (num, noun) {
+    let sent = "On average, a Berkeley student has " + num + " " + noun + "s.";
+    return sent;
+}
+
+function getRandomNum (max) {
+    return Math.floor(Math.random() * max);
+}
+
+let x = getRandomNum(20);
+let y = getRandomNum(10);
+let z = getRandomNum(13);
+var avg = averageThreeNumbers (x, y, z);
+var sentence = createSentence(avg, "quokka");
+console.log(sentence);
